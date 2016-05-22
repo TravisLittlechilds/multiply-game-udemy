05911d3c22b2f957c2871816e71379d14c692755
Travis Littlechilds <travislittlechilds@gmail.com>
2016-05-22 17:00:10 +0100

    Initial Commit

---
+++ Learning Multiply App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learning Multiply App
+//
+//  Created by Travis Littlechilds on 22/05/2016.
+//  Copyright © 2016 Travis Littlechilds. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
